22517216b5b8f22dd70746c865ff744e6d084338
Michael Helgesen <michaelhelgesen02@gmail.com>
2024-03-11 10:03:43 +0100

    Upload relevant files with functional game
    Add a version created in Typescript with very basic styling.

---
+++ script.js
@@ -0,0 +1,26 @@
+var flexContainer = document.querySelector(".flex-container");
+var playButton = document.querySelector(".play-button");
+var validateInput = function () {
+    var input = prompt("Please enter a number between 4 and 100", "16");
+    if (!Number(input) || Number(input) > 100 || Number(input) < 4) {
+        if (confirm("Not a valid number. Please try again")) {
+            return validateInput();
+        }
+    }
+    flexContainer.innerHTML = "";
+    createGrid(Number(input));
+};
+playButton.addEventListener("click", validateInput);
+var createGrid = function (num) {
+    for (var i = num * num; i > 0; i--) {
+        var flexEl = document.createElement("div");
+        flexEl.classList.add("flex-element");
+        flexEl.style.width = "".concat(960 / num, "px");
+        flexEl.style.height = "".concat(960 / num, "px");
+        flexEl.addEventListener("mouseover", function (e) {
+            e.target.classList.add("activated");
+        });
+        flexContainer.appendChild(flexEl);
+    }
+};
+createGrid(16);
---
+++ script.ts
@@ -0,0 +1,30 @@
+const flexContainer: HTMLElement = document.querySelector(".flex-container");
+const playButton: HTMLElement = document.querySelector(".play-button");
+
+const validateInput = () => {
+	const input = prompt("Please enter a number between 4 and 100", "16");
+	if (!Number(input) || Number(input) > 100 || Number(input) < 4) {
+		if(confirm("Not a valid number. Please try again")){
+			return validateInput()
+		}
+	}
+	flexContainer.innerHTML = "";
+	createGrid(Number(input));
+}
+
+playButton.addEventListener("click", validateInput)
+
+const createGrid = (num: number): void => {
+	for(let i = num * num; i > 0; i--){
+		let flexEl = document.createElement("div");
+		flexEl.classList.add("flex-element");
+		flexEl.style.width = `${960 / num}px`
+		flexEl.style.height= `${960 / num}px`
+		flexEl.addEventListener("mouseover", function(e) {
+			(e.target as HTMLElement).classList.add("activated");
+		})
+		flexContainer.appendChild(flexEl);
+	}
+};
+
+createGrid(16);

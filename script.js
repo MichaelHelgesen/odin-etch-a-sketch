5457e59cc5c4e1f048fb7072c3b7978c1ffe054d
Michael Helgesen <michaelhelgesen02@gmail.com>
2024-03-11 13:48:34 +0100

    Adjust the brightness of color on hover
    Adjust the brightness of the colors from 100 to 0 percent and back.
    Changes with 10% on every hover.

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 var flexContainer = document.querySelector(".flex-container");
 var playButton = document.querySelector(".play-button");
+var brightnessLevel = 100;
+var brightnessDirection = 1;
 var validateInput = function () {
     var input = prompt("Please enter a number between 4 and 100", "16");
     if (!Number(input) || Number(input) > 100 || Number(input) < 4) {
@@ -22,8 +24,20 @@ var createGrid = function (num) {
         flexEl.style.height = "".concat(960 / num, "px");
         flexEl.addEventListener("mouseover", function (e) {
             if (!flexEl.classList.contains("activated")) {
-                console.log("false");
                 e.target.style.backgroundColor = "rgb(".concat(createRandomRGB(), ", ").concat(createRandomRGB(), ", ").concat(createRandomRGB(), ")");
+                e.target.style.filter = "brightness(".concat(brightnessLevel, "%)");
+                if (brightnessDirection === 1) {
+                    brightnessLevel -= 10;
+                    if (brightnessLevel === 0) {
+                        brightnessDirection = 0;
+                    }
+                }
+                else {
+                    brightnessLevel += 10;
+                    if (brightnessLevel === 100) {
+                        brightnessDirection = 1;
+                    }
+                }
                 flexEl.classList.add("activated");
             }
         });
--- script.ts
+++ script.ts
@@ -1,6 +1,7 @@
 const flexContainer: HTMLElement = document.querySelector(".flex-container");
 const playButton: HTMLElement = document.querySelector(".play-button");
-
+let brightnessLevel: number  = 100;
+let brightnessDirection: number = 1;
 const validateInput = () => {
 	const input = prompt("Please enter a number between 4 and 100", "16");
 	if (!Number(input) || Number(input) > 100 || Number(input) < 4) {
@@ -23,11 +24,22 @@ const createGrid = (num: number): void => {
 		let flexEl = document.createElement("div");
 		flexEl.classList.add("flex-element");
 		flexEl.style.width = `${960 / num}px`;
-		flexEl.style.height= `${960 / num}px`;	
+		flexEl.style.height= `${960 / num}px`;
 		flexEl.addEventListener("mouseover", function(e) {
 			if(!flexEl.classList.contains("activated")){
-			console.log("false");
 			(e.target as HTMLElement).style.backgroundColor = `rgb(${createRandomRGB()}, ${createRandomRGB()}, ${createRandomRGB()})`;
+			(e.target as HTMLElement).style.filter = `brightness(${brightnessLevel}%)`;
+			if(brightnessDirection === 1) {
+				brightnessLevel -= 10;
+				if (brightnessLevel === 0) {
+					brightnessDirection = 0;
+				}
+			} else {
+				brightnessLevel += 10;
+				if (brightnessLevel === 100) {
+					brightnessDirection = 1;
+				}
+			}
 			flexEl.classList.add("activated");
 		}
 				});
